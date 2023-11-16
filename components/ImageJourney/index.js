5938a8044ab1a60c2caa3807cd7292be2dca320b
Shubham Dhiman <dhimanshubham440818@gmail.com>
2023-11-16 21:46:52 +0530

    image banner

--- components/ImageJourney/index.js
+++ components/ImageJourney/index.js
@@ -1,6 +1,7 @@
 import B1 from 'public/assets/journey_imgs/B1.jpg'
 import B2 from 'public/assets/journey_imgs/B2.jpg'
 import B3 from 'public/assets/journey_imgs/B3.jpg'
+import B4 from 'public/assets/journey_imgs/B4.jpg'
 import b1l1 from 'public/assets/journey_imgs/b1l1.png'
 import b1l2 from 'public/assets/journey_imgs/b1l2.png'
 import b1l3 from 'public/assets/journey_imgs/b1l3.png'
@@ -15,6 +16,11 @@ import b2l1 from 'public/assets/journey_imgs/b2l1.png'
 import b2l2 from 'public/assets/journey_imgs/b2l2.png'
 import b2l3 from 'public/assets/journey_imgs/b2l3.png'
 import b2l4 from 'public/assets/journey_imgs/b2l4.png'
+import b4l1 from 'public/assets/journey_imgs/b4l1.png'
+import b4l2 from 'public/assets/journey_imgs/b4l2.png'
+import b4l3 from 'public/assets/journey_imgs/b4l3.png'
+import b4l4 from 'public/assets/journey_imgs/b4l4.png'
+import b4l5 from 'public/assets/journey_imgs/b4l5.png'
 import style from './style.module.css'
 
 import React, { Component } from 'react';
@@ -37,6 +43,28 @@ export default class Carasol extends Component {
   render() {
     return (
       <Carousel autoPlay={true} showThumbs={false} infiniteLoop={true} interval={2000}>
+                <div className='carousel_banner' style={this.state.imageStyle}>
+          <div className=''>
+            <div className={style.l1}>
+              <img  src={b4l1.src} style={{ width: "100%", height: '100%', border: "5px solid #CC454C", borderRadius: "50%"}} />
+            </div>
+            <div className={style.l2}>
+              <img className='' src={b4l2.src} style={{ width: "100%", height: '100%', border: "5px solid #CC454C", borderRadius: "50%" }} />
+            </div>
+            <div className={style.l3}>
+              <img className='' src={b4l3.src} style={{ width: "100%", height: '100%',border: "5px solid #CC454C", borderRadius: "50%" }} />
+            </div>
+            <div className={style.l4}>
+              <img className='' src={b4l5.src} style={{ width: "100%", height: '100%',border: "5px solid #CC454C", borderRadius: "50%" }} />
+            </div>,
+            <div className={style.l5}>
+              <img className='' src={b4l4.src} style={{ width: "100%", height: '100%',border: "5px solid #CC454C", borderRadius: "50%" }} />
+            </div>
+            <div className=''>
+              <img src={B4.src} style={{ width: "100vw", height: '100vh', }} />
+            </div>
+          </div>
+        </div>
         <div className='carousel_banner' style={this.state.imageStyle}>
           <div className=''>
             <div className={style.l1}>
